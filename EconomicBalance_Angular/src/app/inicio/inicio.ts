47a4adb5085241e8260a50bdebe18d420b691e98
ruben_y_jose <rusaca1984@gmail.com>
2026-02-25 19:06:46 +0100

    Creo Beta DE Inciio Principal

---
+++ EconomicBalance_Angular/src/app/inicio/inicio.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [],
+  standalone: true,
+  templateUrl: './inicio.html',
+  styleUrl: './inicio.css',
+})
+export class Inicio {
+
+}
